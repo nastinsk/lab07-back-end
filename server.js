2e7d53ae6e6bc0aa07492e96d561747aa4674bff
nastinsk <nastinsk@gmail.com>
2019-08-01 19:15:02 -0700

    weather added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+const superagent = require('superagent');
 
 // configure environment variables
 require('dotenv').config();
@@ -26,6 +27,26 @@ app.get('/location', (req,res) => {
   }
 });
 
+app.get('/weather', (request, response) => {
+  try {
+    const weatherData = getWeather();
+    response.send(weatherData);
+  }
+  catch(error) {
+    console.error(error);
+    response.status(500).send('Status: 500. Server error in /weather');
+  }
+});
+
+function getWeather() {
+  const darkskyData = require('./data/darksky.json');
+  const weatherSummaries = [];
+  darkskyData.daily.data.forEach(day => {
+    weatherSummaries.push(new Weather(day));
+  });
+  return weatherSummaries;
+}
+ 
 // constructor function to buld a city object instances
 function City (req, data){
 
@@ -35,6 +56,11 @@ function City (req, data){
   this.longitude = data.results[0].geometry.location.lng;
 }
 
+//weather constructor
+function Weather(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+}
 
 // tell our express server to start listening on port PORT
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
